870c0c14b9809a9b8584cf298e7918259ba65d06
evilom <672246397@qq.com>
2024-06-15 00:41:17 +0800

    fix:read ser line error

--- simple/main_logic.py
+++ simple/main_logic.py
@@ -6,6 +6,8 @@ serName = "/dev/ttyACM0"
 ser = None
 lPos = [0, 0, 0, 0, 0, 0]
 jPos = [0, 0, 0, 0, 0, 0]
+canGetjPOs = False
+canGetLPOs = False
 
 def handle_button_click(button_label):
     axis, action = button_label.split(" ")
@@ -19,14 +21,15 @@ def handle_button_click(button_label):
 def handle_single_axis(axis, action):
     axis_index = int(axis[1:]) - 1
     if action == "+":
-        lPos[axis_index] += 1
+        jPos[axis_index] += 1
         print(f"Incrementing J{axis_index+1}")
     elif action == "-":
-        lPos[axis_index] -= 1
+        jPos[axis_index] -= 1
         print(f"Decrementing J{axis_index+1}")
     else:
         print(f"Invalid action for single axis: {action}")
-    send_command(formatCommand(lPos))
+    print(jPos)
+    send_command(formatCommand(jPos))
 
 def handle_linkage_axis(axis, action):
     if action == "+":
@@ -49,24 +52,33 @@ def connect_to_serial():
         print(f"Failed to connect: {e}")
 
 def send_command(command):
-    global ser
+    global ser,canGetjPOs,canGetLPOs
     if ser and ser.is_open:
         ser.write(f'{command}\n'.encode('utf-8'))
+        if command == '#GETJPOS':
+            canGetjPOs = True
+        elif command == '#GETLPOS':
+            canGetLPOs = True
     else:
         print("Serial port not connected.")
 
 def formatCommand(positions):
-    command_str = "@{},{}\n".format(",".join(map(str, positions)), 5)
-    return command_str.encode()
+    command_str = "&{},{}".format(",".join(map(str, positions)), 5)
+    return command_str
 
 def read_serial_data():
-    global ser
+    global ser,jPos,canGetLPOs,canGetjPOs
     while True:
         if ser and ser.in_waiting > 0:
             data = ser.readline().decode().strip()
             if data:
-                if data.startswith("ok"):
+                if data.startswith("ok") and canGetjPOs:
+                    jPos = parse_line_to_positions(data)
+                    canGetjPOs = False
+                    print(jPos)
+                elif data.startswith("ok") and canGetLPOs:
                     lPos = parse_line_to_positions(data)
+                    canGetLPOs = False
                     print(lPos)
                 print(data)
         time.sleep(0.1)
